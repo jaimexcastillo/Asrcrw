7b2a0a08c330ecf692644830e1e42edb2083b812
jaimexcastillo <jaime22acc@outlook.com>
2022-10-19 10:03:03 -0500

    raiz / para ambos

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "development" ? "/" : "./",
+    publicPath: process.env.NODE_ENV === "development" ? "/" : "/",
     lintOnSave: false,
     css: {
         loaderOptions: {
