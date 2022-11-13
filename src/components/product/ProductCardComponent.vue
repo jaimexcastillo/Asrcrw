8c6e5dba9713dae0ad48f5fdc048b2e59fb6bede
jaimexcastillo <jaime22acc@outlook.com>
2022-11-13 15:56:00 -0600

    rerun all jobs

--- src/components/product/ProductCardComponent.vue
+++ src/components/product/ProductCardComponent.vue
@@ -77,7 +77,7 @@ export default {
     },
     async mounted(){
         await useProductsStore().getProdcuts()
-        this.$forceUpdate()
+        this.$forceUpdate() 
         
     },
     methods:{
